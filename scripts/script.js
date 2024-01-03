cc81eecf5680d6adb567d163a2ef249ed67ada3e
Lyes SID ALI <lyes008007@gmail.com>
2024-01-03 20:16:31 +0100

    nouvelle version finie a 90%

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,10 @@
 
 /* Texte d'acceuil */
 const welcomeText = "Bonjour, moi c'est Lyes SID ALI et bienvenu sur mon porfolio !";
-const contentText = "Vous allez être embarqué dans un univers spécial dans quelques instants, Appuyez sur le bouton ci dessous afin d'accéder au premier niveau pour en savoir plus sur moi."
+const contentText = "Vous allez être embarqué dans un univers spécial dans quelques instants, Appuyez sur le bouton ci dessous afin d'accéder au premier niveau pour en savoir plus sur moi.";
 let welcomeIndex = 0;
 let contentIndex = 0;
+
 
 function typeWelcomeText() {
     if (welcomeIndex < welcomeText.length) {
@@ -109,6 +110,38 @@ document.getElementById("affichee").addEventListener("click", function(event) {
     presentationSection.scrollIntoView({ behavior: "smooth" }); 
 });
 
+
+
+/* Affichages des compétences */
+function verifierReponse(){
+    var rep = document.getElementById("reponse").value;
+    if(rep=="clé" || rep=="cle" || rep=="une clé" || rep=="une cle" || rep=="clef"){
+        document.getElementById("cle").style.display = 'inline-block';
+    }
+    else{
+        alert('Réponse incorrecte. Réessayez');
+    }
+}
+
+function afficheCle(){
+    document.getElementById("cle").style.display = 'inline-block';
+}
+
+document.getElementById("cle").addEventListener("click",function(){
+    document.getElementById("images").style.display = 'inline-block';
+});
+
+
+/* Discours de goku */
+const fin = 'Goku : \n Cher visiteur... vous arrivez au bout de ce voyage, en espérant que vous avez apprécié. \n Je vous laisse à présent découvrir mes contacts en appuyant sur le bouton ci dessous.'
+let finIndex = 0;
+function typeFinText() {
+    if (finIndex < fin.length) {
+        document.getElementById("fin").textContent += fin.charAt(finIndex);
+        finIndex++;
+        setTimeout(typeFinText,30); 
+    }
+}
 
 /* Sticky header */
 window.onscroll = function() {myFunction()};
